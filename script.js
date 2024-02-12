463ae78e325f8e345ba8e8ee047216cd762f650d
P7qMXFXqzy <fbmz4ekslh@gmail.com>
2024-02-11 21:14:12 -0300

    Atualização da página de login, adicionada a conexão com banco de dados e redirecionamento para a página inicial caso o login esteja correto

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const port = 8000;
 const path = require('path');
 var bodyParser = require( 'body-parser' )
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+//executar conexão com o banco de dados
 const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
 const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri);
-async function run() {
+//variável que armazenará os dados do usuário conectado
+let usuarioConectado = null;
+
+//buscar usuário no banco de dados através do nome e senha inseridos
+async function login(usuarioInserido, senhaInserida) {
+  const client = new MongoClient(uri);
   try {
-    const database = client.db('kartodromo');
-    const movies = database.collection('usuarios');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { _id: '74394' };
-    const movie = await movies.findOne({ _id: '74394' });
-    console.log(movie["_id"]);
-  } 
+    const bdKartodromo = client.db('kartodromo');
+    const colecao = bdKartodromo.collection('usuarios');
+    const usuarioEncontrado = await colecao.findOne({ nome: usuarioInserido, senha: senhaInserida});
+    return usuarioEncontrado;
+  }
   finally {await client.close();}
 }
-run().catch(console.dir);
-
 
 
 //ativar arquivos css
-app.use("/login", express.static(path.join(__dirname, "outrosArquivos\\css")));
+app.use(["/login","/pagina2"], express.static(path.join(__dirname, "outrosArquivos\\css")));
 
+//mandar arquivo html quando usuário acessar a página
 app.get("/login", (req,res) => {
   res.sendFile(path.join(__dirname, "\\outrosArquivos\\paginaLogin.html"));
 })
-app.post("/login", (req,res) => {
-  console.log('a');
-  res.status(204).send();
+app.get("/pagina_inicial", (req,res) => {
+  res.sendFile(path.join(__dirname, "\\outrosArquivos\\paginaInicial.html"));
 })
 
+//realizar login na página "login"
+app.post("/login", (req,res) => {
+  const usuarioInserido = req.body.inputNome;
+  const senhaInserido = req.body.inputSenha;
+  usuarioConectado = login(usuarioInserido, senhaInserido);
+  usuarioConectado.then(function(result){
+    if(result != null){
+      res.redirect("/pagina_inicial");
+    }
+    else{
+      res.alert("aaa").send();
+      res.status(204).send();
+    }
+  })
+  
+})
   
 //executar servidor na porta 3000
 app.listen(port, () => {
